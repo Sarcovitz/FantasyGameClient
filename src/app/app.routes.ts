5ea41eb5ef7f2864104ca122164e06614b375930
Filip Sarkowicz <fsarkowicz@gmail.com>
2024-06-14 10:37:51 +0200

    app routes for register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/pages/home/home.component';
+import { RegisterComponent } from './features/auth/register/register.component';
 
 export const routes: Routes = [
     {path:'', redirectTo: 'home', pathMatch: 'full'},
     {path:'home', component: HomeComponent},
+    {path:'auth', children: [
+            {path:'register', component: RegisterComponent}
+        ]
+    }
 ];
