7ac4bfd6b6ac6347036721eaf87b4e6efcf2cd2f
Filip Sarkowicz <fsarkowicz@gmail.com>
2024-06-15 11:10:05 +0200

    RouterModule in topbar

--- src/app/core/shared/layout/topbar/topbar.component.ts
+++ src/app/core/shared/layout/topbar/topbar.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { LayoutModule } from '../layout.module';
 
 @Component({
   selector: 'app-topbar',
   standalone: true,
   templateUrl: './topbar.component.html',
-  styleUrl: './topbar.component.scss'
+  styleUrl: './topbar.component.scss',
+  imports: [LayoutModule]
 })
 export class TopbarComponent {
 
