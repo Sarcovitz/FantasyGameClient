1e346fb10cc3888249d9a49c469f57660d2652d8
Filip Sarkowicz <fsarkowicz@gmail.com>
2024-06-15 12:55:51 +0200

    registerComponent registerForm

--- src/app/features/auth/register/register.component.ts
+++ src/app/features/auth/register/register.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,6 +8,30 @@ import { Component } from '@angular/core';
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  registerForm = new FormGroup({
+		username: new FormControl('', [
+			Validators.required,
+			Validators.minLength(3),
+			Validators.maxLength(16),
+			Validators.pattern(/^[a-zA-Z0-9]+$/),
+		]),
+		email: new FormControl('', [
+			Validators.required,
+      Validators.email,
+		]),
+		password: new FormControl('', [
+			Validators.required,
+			Validators.minLength(6),
+		]),
+		password2: new FormControl('', [
+			Validators.required,
+			Validators.minLength(6),
+		]),
+	});
 
+  constructor() {}
+
+  ngOnInit(): void {
+  }
 }
