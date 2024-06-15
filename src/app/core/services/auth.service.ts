81457ac50ee978206af940b2d93dae3084b7bd1a
Filip Sarkowicz <fsarkowicz@gmail.com>
2024-06-15 17:01:01 +0200

    raw register method in auth service

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -6,4 +6,21 @@ import { Injectable } from '@angular/core';
 export class AuthService {
 
   constructor() { }
+
+  register(registerUserRequest: RegisterUserRequest): void {
+		this.isLoading = true;
+		this.api.post<RegisterUserDTO>('/auth/register', registerUserRequest).subscribe({
+			next: response => {
+				let userdata: string = response.body?.username + ':' + response.body?.id + ':' + response.body?.email;
+				userdata = Buffer.from(userdata).toString('base64');
+
+				this.isLoading = false;
+				this.router.navigateByUrl('/auth/registered/' + encodeURI(userdata));
+			},
+			error: error => {
+				this.registerErrorMessage = error.error;
+				this.isLoading = false;
+			},
+		});
+	}
 }
